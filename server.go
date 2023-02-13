a7cca3a906ee1f803973d0fed9ad90ab4f2cfb76
Taishi-Ito <taishi.ito1026@gmail.com>
2023-02-13 15:38:38 +0900

    feat:Install air

--- server.go
+++ server.go
@@ -3,9 +3,15 @@ package main
 import(
 	"github.com/labstack/echo/v4"
 	// "github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 func main() {
 	e := echo.New()
+	e.GET("/", hello)
 	e.Logger.Fatal(e.Start(":8081"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!!!!")
 }
