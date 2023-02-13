7111cf9b398a36cb4010ee63849c08d0abcbabcc
Taishi-Ito <taishi.ito1026@gmail.com>
2023-02-13 22:21:42 +0900

    feat:Install logger

--- server.go
+++ server.go
@@ -5,10 +5,12 @@ import(
 	"github.com/labstack/echo/v4"
 	// "github.com/labstack/echo/v4/middleware"
 	"net/http"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	routes.Init(e)
 	e.GET("/", hello)
 	e.Logger.Fatal(e.Start(":8081"))
