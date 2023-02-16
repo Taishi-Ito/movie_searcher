d14fca186c196eb9f9eb9638a8f812fb47d943cf
Taishi-Ito <taishi.ito1026@gmail.com>
2023-02-16 09:01:31 +0900

    feat:Create middleware to set db connection

---
+++ middlewares/database.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"movie_searcher/databases"
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo/v4"
+)
+
+type DatabaseClient struct {
+	DB *gorm.DB
+}
+
+func DatabaseService() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			session, _ := database.Connect()
+			d := DatabaseClient{DB: session}
+
+			defer d.DB.Close()
+
+			d.DB.LogMode(true)
+			c.Set("dbs", &d)
+
+			if err := next(c); err != nil {
+				return err
+			}
+
+			return nil
+		}
+	}
+}
