35996cc81b63590559a230758f1f30b5188945ac
Taishi-Ito <taishi.ito1026@gmail.com>
2023-02-16 09:13:37 +0900

    feat:Modify FetchSimilarMovies() to get movies title

--- web/api/fetch_similar_movies.go
+++ web/api/fetch_similar_movies.go
@@ -3,10 +3,16 @@ package api
 import(
 	"github.com/labstack/echo/v4"
 	"github.com/valyala/fasthttp"
+	"movie_searcher/models"
+	"movie_searcher/middlewares"
 )
 
 func FetchSimilarMovies() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.JSON(fasthttp.StatusOK, "Most similar movies")
+		dbs := c.Get("dbs").(*middlewares.DatabaseClient)
+		movies := []models.Movie{}
+		// dbs.DB.Find(&movies)
+		dbs.DB.Debug().Select("title").Find(&movies)
+		return c.JSON(fasthttp.StatusOK, movies)
 	}
 }
