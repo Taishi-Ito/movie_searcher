e27f7388dabdd087ee693965a7d5dd3a07720e33
Taishi-Ito <taishi.ito1026@gmail.com>
2023-02-26 10:54:56 +0900

    refactor:Delete unnecessary code

--- web/api/fetch_movie_detail.go
+++ web/api/fetch_movie_detail.go
@@ -13,7 +13,6 @@ func FetchMovieDetail() echo.HandlerFunc {
 		id := c.Param("id")
 		dbs := c.Get("dbs").(*middlewares.DatabaseClient)
 		movie := models.Movie{}
-		// dbs.DB.Debug().Where("id = ?", id).First(&movie)
 		dbs.DB.Debug().Select([]string{"title", "year", "summary", "imdb_rank", "prime_rating", "imdb_rating", "average_rating", "prime_id", "prime_review_num", "film_length"}).Where("id = ?", id).First(&movie)
 		fmt.Println(movie)
 		return c.JSON(fasthttp.StatusOK, movie)
