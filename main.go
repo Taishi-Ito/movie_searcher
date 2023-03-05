d086f04abbd78cf2760f335dcd74aa9f4ede774f
Taishi-Ito <taishi.ito1026@gmail.com>
2023-03-05 15:17:40 +0900

    refactor:Delete unnecessary code

--- main.go
+++ main.go
@@ -35,16 +35,6 @@ func main() {
 
 	// Routes
 	routes.Init(e)
-	// switch env {
-    // case "prod":
-    //     e.Logger.Fatal(e.StartAutoTLS(":443"))
-    // case "dev":
-    //     defaultAddr := ":80"
-    //     e.Logger.Fatal(e.Start(defaultAddr))
-    // default:
-    //     defaultAddr := ":8081"
-    //     e.Logger.Fatal(e.Start(defaultAddr))
-    // }
 	switch env {
     case "prod":
         e.Logger.Fatal(e.StartAutoTLS(":443"))
@@ -52,5 +42,4 @@ func main() {
         defaultAddr := ":8081"
         e.Logger.Fatal(e.Start(defaultAddr))
     }
-	// e.Logger.Fatal(e.Start(":8081"))
 }
